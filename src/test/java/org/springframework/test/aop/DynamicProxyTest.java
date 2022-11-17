7355d23c372df04e94cecd3fbf18f3f6dd9ac3f8
Songzm1999 <1547726250@qq.com>
2022-11-17 18:28:35 +0800

    增加一个方法具有多个增强功能

--- src/test/java/org/springframework/test/aop/DynamicProxyTest.java
+++ src/test/java/org/springframework/test/aop/DynamicProxyTest.java
@@ -1,5 +1,6 @@
 package org.springframework.test.aop;
 
+import org.aopalliance.intercept.MethodInterceptor;
 import org.junit.Test;
 import org.springframework.aop.AdvisedSupport;
 import org.springframework.aop.MethodMatcher;
@@ -7,6 +8,7 @@ import org.springframework.aop.TargetSource;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.framework.JdkDynamicAopProxy;
 import org.springframework.test.common.WorldServiceInterceptor;
+import org.springframework.test.common.WorldServiceInterceptor2;
 import org.springframework.test.service.WorldService;
 import org.springframework.test.service.WorldServiceImpl;
 
@@ -22,13 +24,16 @@ public class DynamicProxyTest {
 
 		AdvisedSupport advisedSupport = new AdvisedSupport();
 		TargetSource targetSource = new TargetSource(worldService);
-		WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
 		MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* org.springframework.test.service.WorldService.explode(..))").getMethodMatcher();
 		advisedSupport.setTargetSource(targetSource);
-		advisedSupport.setMethodInterceptor(methodInterceptor);
 		advisedSupport.setMethodMatcher(methodMatcher);
 
+		MethodInterceptor methodInterceptor = new WorldServiceInterceptor();
+		MethodInterceptor methodInterceptor2 = new WorldServiceInterceptor2();
+		advisedSupport.getMethodInterceptorList().add(methodInterceptor);
+		advisedSupport.getMethodInterceptorList().add(methodInterceptor2);
+
 		WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
-		proxy.explode();
+		System.out.println(proxy.explode());
 	}
 }
--- src/test/java/org/springframework/test/service/WorldServiceImpl.java
+++ src/test/java/org/springframework/test/service/WorldServiceImpl.java
@@ -7,7 +7,8 @@ package org.springframework.test.service;
 public class WorldServiceImpl implements WorldService {
 
 	@Override
-	public void explode() {
+	public boolean explode() {
 		System.out.println("The Earth is going to explode");
+		return true;
 	}
 }
