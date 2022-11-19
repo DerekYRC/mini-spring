90a49f62aa7cf22af2c361570c780f65f84d0aef
FranzLi347 <ingachi@163.com>
2022-11-19 11:06:33 +0800

    fix:更正DefaultListableBeanFactory#getBeansOfType中错误

--- src/main/java/org/springframework/beans/factory/support/DefaultListableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/DefaultListableBeanFactory.java
@@ -40,13 +40,14 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
 	@Override
 	public <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
 		Map<String, T> result = new HashMap<>();
-		beanDefinitionMap.forEach((beanName, beanDefinition) -> {
-			Class beanClass = beanDefinition.getBeanClass();
-			if (type.isAssignableFrom(beanClass)) {
+		for (Map.Entry<String, BeanDefinition> beanDefinitionWithName : beanDefinitionMap.entrySet()) {
+			String beanName = beanDefinitionWithName.getKey();
+			BeanDefinition bd = beanDefinitionWithName.getValue();
+			if(type.isAssignableFrom(bd.getBeanClass())){
 				T bean = (T) getBean(beanName);
-				result.put(beanName, bean);
+				result.put(beanName,bean);
 			}
-		});
+		}
 		return result;
 	}
 
