a5830ce504e5aded81567422564c127c16be1944
宁猫 <3113143862@qq.com>
2025-08-20 16:39:16 +0800

    feat: 为StringToNumberConverterFactory实现了提供对多个Number类型的转换

--- src/main/java/org/springframework/core/convert/support/StringToNumberConverterFactory.java
+++ src/main/java/org/springframework/core/convert/support/StringToNumberConverterFactory.java
@@ -3,6 +3,9 @@ package org.springframework.core.convert.support;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  * @author derekyi
  * @date 2021/1/10
@@ -24,20 +27,58 @@ public class StringToNumberConverterFactory implements ConverterFactory<String, 
 
 		@Override
 		public T convert(String source) {
-			if (source.length() == 0) {
+			// 处理null和空字符串
+			if (source == null || source.isEmpty()) {
 				return null;
 			}
 
-			if (targetType.equals(Integer.class)) {
-				return (T) Integer.valueOf(source);
-			} else if (targetType.equals(Long.class)) {
-				return (T) Long.valueOf(source);
+			// 去除首尾空白字符
+			source = source.trim();
+			if (source.isEmpty()) {
+				return null;
 			}
-			//TODO 其他数字类型
 
-			else {
+			try {
+				// Integer类型转换
+				if (targetType.equals(Integer.class)) {
+					return (T) Integer.valueOf(source);
+				}
+				// Long类型转换
+				else if (targetType.equals(Long.class)) {
+					return (T) Long.valueOf(source);
+				}
+				// Short类型转换
+				else if (targetType.equals(Short.class)) {
+					return (T) Short.valueOf(source);
+				}
+				// Byte类型转换
+				else if (targetType.equals(Byte.class)) {
+					return (T) Byte.valueOf(source);
+				}
+				// Float类型转换
+				else if (targetType.equals(Float.class)) {
+					return (T) Float.valueOf(source);
+				}
+				// Double类型转换
+				else if (targetType.equals(Double.class)) {
+					return (T) Double.valueOf(source);
+				}
+				// BigInteger类型转换
+				else if (targetType.equals(BigInteger.class)) {
+					return (T) new BigInteger(source);
+				}
+				// BigDecimal类型转换
+				else if (targetType.equals(BigDecimal.class)) {
+					return (T) new BigDecimal(source);
+				}
+				// 不支持的数字类型
+				else {
+					throw new IllegalArgumentException(
+							"Cannot convert String [" + source + "] to target class [" + targetType.getName() + "]");
+				}
+			} catch (NumberFormatException ex) {
 				throw new IllegalArgumentException(
-						"Cannot convert String [" + source + "] to target class [" + targetType.getName() + "]");
+						"Cannot convert String [" + source + "] to target class [" + targetType.getName() + "]", ex);
 			}
 		}
 	}
